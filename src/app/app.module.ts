1cc3512c9ab47bd8d91ba01b3f7bf2f4aef2df64
f-chilmi <68938896+f-chilmi@users.noreply.github.com>
2021-03-21 13:49:00 +0700

    layouting login and register views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
